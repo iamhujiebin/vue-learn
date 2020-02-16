f8b371fea0dbbab257e0183b05057e4dede860c6
jiebin.hu <458249864@qq.com>
2020-02-16 23:55:11 +0800

    动态路由。默认hash模式

--- vue-mall/src/router/index.js
+++ vue-mall/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from "../components/HelloWorld"
+import GoodList from "../views/GoodList"
 
 Vue.use(Router)
 
-export default new Router({
+export default new Router({//事实上是对console中api ：history的封装
+  mode:'hash',//默认hash，有#；还有history，需要后端支持
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/goods/:goodsId/user/:userId',
+      name:'GoodList',
+      component:GoodList,
     }
   ]
 })
---
+++ vue-mall/src/views/GoodList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      这是商品列表
+      <span>{{$route.params.goodsId}}</span>
+      <span>{{$route.params.userId}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GoodList"
+    }
+</script>
+
+<style scoped>
+
+</style>
